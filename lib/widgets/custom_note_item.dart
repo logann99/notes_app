7d40e209d5298d648a9447cd58461f6d2b555784
ramadan <ahmed.201317499@gmail.com>
2022-12-26 20:27:13 +0200

    Create edit note View

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,70 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
+
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(
-          0xffFFCC80,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
         ),
-        borderRadius: BorderRadius.circular(
-          16,
+        decoration: BoxDecoration(
+          color: const Color(
+            0xffFFCC80,
+          ),
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                bottom: 16,
-              ),
-              child: Text(
-                'Build Your career with Ahmed Ramadan',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(.4),
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  bottom: 16,
+                ),
+                child: Text(
+                  'Build Your career with Ahmed Ramadan',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.4),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+              ),
+              child: Text(
+                'Desc 18 ,2022',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black.withOpacity(.4),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 16,
-            ),
-            child: Text(
-              'Desc 18 ,2022',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withOpacity(.4),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
