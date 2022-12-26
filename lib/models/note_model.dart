c9e723276c3531050741ced1ecf05c86981d039a
ramadan <ahmed.201317499@gmail.com>
2022-12-26 23:20:14 +0200

    add hive

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -4,6 +4,9 @@ class NoteModel {
   final String date;
   final int color;
 
-  NoteModel({required this.title,required this.subTitle,required this.date,required this.color});
-  
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
 }
