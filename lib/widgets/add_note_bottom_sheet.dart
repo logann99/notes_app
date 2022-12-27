63d9475968c0cc4915aac4ee9d7328728d2a7b88
ramadan <ahmed.201317499@gmail.com>
2022-12-27 02:14:39 +0200

    Validate user input

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -2,36 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
+class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
+    return const Padding(
+      padding: EdgeInsets.symmetric(
         horizontal: 16,
       ),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
-        children: const [
-          SizedBox(
+        children: [
+          const SizedBox(
             height: 32,
           ),
           CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
             hint: 'Title',
             maxLines: 2,
           ),
-          SizedBox(
+          const SizedBox(
             height: 16,
           ),
           CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
             hint: 'Content',
             maxLines: 8,
           ),
-          SizedBox(
+          const SizedBox(
             height: 32,
           ),
-          CustomButton(),
-          SizedBox(
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {
+                  
+                });
+              }
+            },
+          ),
+          const SizedBox(
             height: 16,
           ),
         ],
