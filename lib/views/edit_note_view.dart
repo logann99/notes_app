d2937857d6f84fd138a7fee9788f12e52eab65af
ramadan <ahmed.201317499@gmail.com>
2023-01-10 13:27:08 +0200

    Edit note done

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/edit_view_body.dart';
 
-class EditNoteView extends StatelessWidget {
-  const EditNoteView({super.key});
+import '../models/note_model.dart';
 
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key, required this.note});
+
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: EditNoteViewBody(),
+    return Scaffold(
+      body: EditNoteViewBody(
+        note: note,
+      ),
     );
   }
 }
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 import 'custom_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title, required this.icon});
+  const CustomAppBar(
+      {super.key, required this.title, required this.icon, this.onPressed});
+
   final String title;
   final IconData icon;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children:  [
+      children: [
         Text(
           title,
           style: const TextStyle(
@@ -20,6 +23,7 @@ class CustomAppBar extends StatelessWidget {
         ),
         const Spacer(),
         CustomIcon(
+          onPressed: onPressed,
           icon: icon,
         ),
       ],
